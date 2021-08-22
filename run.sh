e136b5f95497f2240444b7c590ce99dd0c297b34
paralainer <serg.talov@gmail.com>
2021-08-22 19:41:34 +1000

    name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker build -t ps5-app . && docker run -d --restart=on-failure --env-file ./env ps5-app
+docker build -t ps5-app . && docker run -d --restart=on-failure --name ps5-app --env-file ./env ps5-app
